d29bc55adc2690f00e87829d358acd25d5189629
KadaliPrasAnthi <kadaliprasanthi87@gmail.com>
2026-08-01 22:13:33 +0530

    fixing issues

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,20 @@ import NotFound from './components/NotFound'
 import './App.css'
 
 const App=()=>{
-  
-  return (<BrowserRouter>
-  <Routes>
+  return     <div style={{ padding: "20px" }}>
+      <h1>Hello Vercel</h1>
+    </div>
+
+  // return (<BrowserRouter>
+  // <Routes>
    
-     <Route path="/login" element={<Login/>}/>
-     {/* <Route path="/" element={<MainMenu/>}/> */}
-    <Route path="/" element={<ProtectedRoute><MainMenu/></ProtectedRoute>}/>
-     <Route path="*" element={<NotFound/>}/>
-  </Routes>
+  //    <Route path="/login" element={<Login/>}/>
+  //    {/* <Route path="/" element={<MainMenu/>}/> */}
+  //   <Route path="/" element={<ProtectedRoute><MainMenu/></ProtectedRoute>}/>
+  //    <Route path="*" element={<NotFound/>}/>
+  // </Routes>
   
-  </BrowserRouter>)
+  // </BrowserRouter>)
    
 }
 
