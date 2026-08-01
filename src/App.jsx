60ff83e65ee9f543fbc94bbee3de3a4520e64700
KadaliPrasAnthi <kadaliprasanthi87@gmail.com>
2026-08-01 21:13:15 +0530

    changed configurations

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,22 @@ import {BrowserRouter,Route,Routes} from 'react-router-dom'
 import Login from './components/Login'
 import ProtectedRoute  from './components/ProtectedRoute'
 import MainMenu from './components/MainMenu'
+import NotFound from './components/NotFound'
 
 import './App.css'
 
 const App=()=>{
-  return <BrowserRouter>
+  
+  return (<BrowserRouter>
   <Routes>
-    <Route path="/login" element={<Login/>}/>
+   
+     <Route path="/login" element={<Login/>}/>
+     {/* <Route path="/" element={<MainMenu/>}/> */}
     <Route path="/" element={<ProtectedRoute><MainMenu/></ProtectedRoute>}/>
+     <Route path="*" element={<NotFound/>}/>
   </Routes>
   
-  </BrowserRouter>
+  </BrowserRouter>)
    
 }
 
